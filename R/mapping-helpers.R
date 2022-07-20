1700ea0976e0d9e98ff44215a8ab737ca4ce982e
McDonald <Kira.McDonald@aecom.com>
2022-07-20 11:41:29 -0400

    added option for bbox2ggcrop

--- R/mapping-helpers.R
+++ R/mapping-helpers.R
@@ -234,8 +234,11 @@ get.stamen.bkg <- function(sfx
 #' Wraps `coord_sf`, which can be used to crop a map made in ggplot. Uses an sf object
 #' or bbox to set crop area.
 #'
+#' @param sfx bbox or sf object
+#' @param clip passed onto `ggplot2::coord_sf`
+#'
 #' @export bbox2ggcrop
-bbox2ggcrop <- function(sfx, crs = 4326) {
+bbox2ggcrop <- function(sfx, crs = 4326, clip = 'on') {
 
   # turn sf to bbox if needed
   if(! 'bbox' %in% class(sfx))
@@ -244,8 +247,8 @@ bbox2ggcrop <- function(sfx, crs = 4326) {
 
   # return coord_sf
   ggplot2::coord_sf( xlim = c(sfx[['xmin']], sfx['xmax'])
-                     ,ylim = c(sfx[['ymin']], sfx['ymax']))
-
+                     ,ylim = c(sfx[['ymin']], sfx['ymax'])
+                     ,clip = clip)
 }
 
 
